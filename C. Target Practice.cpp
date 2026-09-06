79f401ac9be1be5a4543e9df12e977ee437e2a51
Suryansh Tiwari <suryansheng456@gmail.com>
2026-09-06 18:06:46 +0530

    Add files via upload

---
+++ C. Target Practice.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int ans = 0;
+
+        for (int i = 0; i < 10; ++i) {
+            string s;
+            cin >> s;
+
+            for (int j = 0; j < 10; ++j) {
+                if (s[j] == 'X') {
+                    ans += min({i, j, 9 - i, 9 - j}) + 1;
+                }
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
