8f93b279305dd02375898f616c7dadb6d9b46f6f
Suryansh Tiwari <suryansheng456@gmail.com>
2026-09-06 17:30:22 +0530

    Add files via upload

---
+++ A. Goals of Victory.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, sum = 0, x;
+        cin >> n;
+
+        for (int i = 0; i < n - 1; ++i) {
+            cin >> x;
+            sum += x;
+        }
+
+        cout << -sum << '\n';
+    }
+}
