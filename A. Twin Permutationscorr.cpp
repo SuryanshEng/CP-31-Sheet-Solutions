3ec8310f058aee66e2dbd7315379c690a6738c51
Suryansh Tiwari <suryansheng456@gmail.com>
2026-09-12 23:33:34 +0530

    Add files via upload

---
+++ A. Twin Permutationscorr.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        for (int i = 0, x; i < n; i++) {
+            cin >> x;
+            cout << n + 1 - x << ' ';
+        }
+        cout << '\n';
+    }
+}
