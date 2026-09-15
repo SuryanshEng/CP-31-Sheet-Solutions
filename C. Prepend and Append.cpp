06795ae92a4760a4b71b99de4290540fd99e0c52
Suryansh Tiwari <suryansheng456@gmail.com>
2026-09-15 06:53:33 +0530

    Add files via upload

---
+++ C. Prepend and Append.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; string s;
+        cin >> n >> s;
+        int i = 0, j = n - 1;
+        while (i < j && s[i] != s[j]) { i++; j--; }
+        cout << (j - i + 1) << "\n";
+    }
+}
