16c3e362d76f7c2ff1be1824f0bf68a3e00a01bf
Suryansh Tiwari <suryansheng456@gmail.com>
2026-09-20 21:43:00 +0530

    Add files via upload

---
+++ C. Vasilije in Cacak.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        ll n, k, x;
+        cin >> n >> k >> x;
+
+        ll mn = k * (k + 1) / 2;
+        ll mx = k * (2 * n - k + 1) / 2;
+
+        cout << (mn <= x && x <= mx ? "YES\n" : "NO\n");
+    }
+}
