b6881319151d480b2829532129a7241dbbf85030
Suryansh Tiwari <suryansheng456@gmail.com>
2026-09-21 15:37:19 +0530

    Add files via upload

---
+++ A. Make It Zero.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        for (int i = 0, x; i < n; ++i) cin >> x;
+
+        if (n % 2 == 0) {
+            cout << 2 << '\n';
+            cout << 1 << ' ' << n << '\n';
+            cout << 1 << ' ' << n << '\n';
+        } else {
+            cout << 4 << '\n';
+            cout << 1 << ' ' << n - 1 << '\n';
+            cout << 1 << ' ' << n - 1 << '\n';
+            cout << n - 1 << ' ' << n << '\n';
+            cout << n - 1 << ' ' << n << '\n';
+        }
+    }
+}
